6de7c804d2503df0ad53ac6c367162438c845ee2
Leonardo Szarblewski <leonardo27.laurindo@gmail.com>
2025-01-10 22:55:39 -0300

    Obtendo a moeda selecionada

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
+const form = document.querySelector("form")
 
-
+//Valida para apenas aparecer números no input
 amount.addEventListener("input", () => {
-
-    //Valida para apenas aparecer números no input
     const hasCharacterRegex = /\D+/g
     amount.value = amount.value.replace(hasCharacterRegex, "")
 })
+
+//Capturando o evento submit do formulário
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+    currency.value  
+})
+
