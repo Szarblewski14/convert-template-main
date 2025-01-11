6ccd3b2e6deaa4016fb1e2621753d498bf743914
Leonardo Szarblewski <leonardo27.laurindo@gmail.com>
2025-01-11 11:07:09 -0300

    Identificando a moeda

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+// Cotação da moeda
+const USD = 6.10
+const EUR = 6.26
+const GBP = 7.4
+
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
 const form = document.querySelector("form")
+
 
 //Valida para apenas aparecer números no input
 amount.addEventListener("input", () => {
@@ -11,6 +17,21 @@ amount.addEventListener("input", () => {
 //Capturando o evento submit do formulário
 form.addEventListener("submit", (e) => {
     e.preventDefault()
-    currency.value  
+
+    switch (  currency.value ) {
+        case "USD":
+            convertCurrency(amount.value, USD, "US$")
+            break;
+        case "EUR":
+            convertCurrency(amount.value, EUR, "€")
+            break
+        case "GBP":
+            convertCurrency(amount.value, GBP, "£")
+            break
+    } 
 })
 
+// Convertendo moeda
+function convertCurrency(amount, price, symbol){
+    console.log(amount, price, symbol);
+}
