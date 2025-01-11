55dd7de91812272c34ff475574bfadc219efc9e3
Leonardo Szarblewski <leonardo27.laurindo@gmail.com>
2025-01-11 11:12:23 -0300

    Exibindo o footer

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const GBP = 7.4
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
 const form = document.querySelector("form")
+const footer = document.querySelector("main footer")
 
 
 //Valida para apenas aparecer números no input
@@ -33,5 +34,13 @@ form.addEventListener("submit", (e) => {
 
 // Convertendo moeda
 function convertCurrency(amount, price, symbol){
-    console.log(amount, price, symbol);
+    try {
+        // Aplica uma classe que exibe o footer
+        footer.classList.add("show-result")
+    } catch (error) {
+        // Remove uma classe que exibe o footer
+        console.log(error);
+        alert("Não foi possivel converter")
+        footer.classList.remove("show-result")
+    }
 }
